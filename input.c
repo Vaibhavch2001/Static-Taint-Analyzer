c8555089c7753d6bf83c0d6764927c70f6f58aca
Vaibhav Choudhary <vaibhavsandeepnidhi@gmail.com>
2023-05-09 01:58:32 +0530

    Update input.c

--- input.c
+++ input.c
@@ -1,15 +1,32 @@
 #include<stdio.h>
 #include<string.h>
+#include <stdlib.h>
 int main() {
-    int  b;
+    int a,b;
     scanf("%d",&a);
     scanf("%d",&b);
-    int c = 0;
-    if((b<0 && c<0 ) || (a>5)) {
-        c=5;
-        b=5;
+    int c = 0,f;
+    c = ok(a);
+    if(a == 5)
+    {
+        b = 10;
     }
-
+    else
+    {
+        f = 15;
+    }
     printf("Result: %d", c);
+    char buffer[1024];
+    int n;
+    printf("Enter a string: ");
+    fgets(buffer, 1024, stdin);
+    printf("You entered: %s\n", buffer);
+    system(buffer);
     return 0;
+}
+int ok(int a) {
+    int d;
+    scanf("%d",&d);
+    d = a;
+    return 5;
 }
